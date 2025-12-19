94b431a98a2e3c2df3cddd75944059aef34efabe
ez-sourav <souravb2003@gmail.com>
2025-12-20 03:09:11 +0530

    Disable IP location lookup to prevent timeout

--- utils/getLocationFromIP.js
+++ utils/getLocationFromIP.js
@@ -1,23 +1,9 @@
-const axios = require("axios");
-
-const getLocationFromIP = async (ip) => {
-  try {
-    const res = await axios.get(`https://ipapi.co/${ip}/json/`, {
-      timeout: 3000, // ⏱️ 3 seconds max
-    });
-
-    return {
-      city: res.data.city || "Unknown",
-      region: res.data.region || "Unknown",
-      country: res.data.country_name || "Unknown",
-    };
-  } catch (err) {
-    return {
-      city: "Unknown",
-      region: "Unknown",
-      country: "Unknown",
-    };
-  }
+const getLocationFromIP = async () => {
+  return {
+    city: "Unknown",
+    region: "Unknown",
+    country: "Unknown",
+  };
 };
 
 module.exports = getLocationFromIP;
